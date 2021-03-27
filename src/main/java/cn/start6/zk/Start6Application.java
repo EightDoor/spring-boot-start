4b410ca501a5d4b99f4b609015e38f6e07c6d1fe
EightDoor <zhou851708184@163.com>
2021-03-27 22:24:28 +0800

    init

---
+++ src/main/java/cn/start6/zk/Start6Application.java
@@ -0,0 +1,13 @@
+package cn.start6.zk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Start6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Start6Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/start6/zk/Start6ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.start6.zk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Start6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
